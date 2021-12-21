0cf628e46fa7ca80b557d5a0701fa994fee4ab09
Yann Bemba <yann@soloso.fr>
2021-12-21 20:29:45 +0100

    admin panel

---
+++ lib/main.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const PanelAdminApp());
+}
+
+class PanelAdminApp extends StatelessWidget {
+  const PanelAdminApp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+
+    );
+  }
+}
